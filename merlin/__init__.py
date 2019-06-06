6db90365ad0dbc8b037f7223f4c6267b88feaf35
mental <m3nta1@yahoo.com>
2019-06-06 06:15:05 +0100

    Shadow imported names

--- merlin/__init__.py
+++ merlin/__init__.py
@@ -1,6 +1,6 @@
-import asyncio
-import logging
-from typing import NoReturn
+import asyncio as _asyncio
+import logging as _logging
+from typing import NoReturn as _NoReturn
 
 from . import core
 from .core import Service
@@ -8,23 +8,23 @@ from .core import Service
 __author__ = 'mental'
 __version__ = '0.1.0'
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)  # TODO: Setup proper logging environment.
+_logging.basicConfig(format='[%(levelname)s] %(message)s', level=_logging.INFO)  # TODO: Setup proper logging environment.
 
-def spawn(*args, **kwargs) -> NoReturn:
+def spawn(*args, **kwargs) -> _NoReturn:
     """Start and spin a merlin client and block until it dies."""
     backend = core.backend.load(kwargs['config']['app']['backend'])
 
-    loop = asyncio.get_event_loop()
+    loop = _asyncio.get_event_loop()
     client = backend.Client(loop=loop)
 
-    logging.info(f'Spawining client: {client!r}')
+    _logging.info(f'Spawining client: {client!r}')
 
     try:
         loop.create_task(client.spawn(*args, **kwargs))
         loop.run_forever()
     except Exception:
         if loop.is_closed():
-            logging.warn(f'Eventloop is closed! `Client.shutdown` was not called.')
+            _logging.warn(f'Eventloop is closed! `Client.shutdown` was not called.')
             return
 
         # TODO: Gracefully shutdown the event loop
