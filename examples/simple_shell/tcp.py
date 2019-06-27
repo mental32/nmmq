83f101d88521cff450cfe2adf16254c16d6f2b0e
mental <m3nta1@yahoo.com>
2019-06-27 05:18:22 +0100

    Document simple_shell example code

--- examples/simple_shell/tcp.py
+++ examples/simple_shell/tcp.py
@@ -6,6 +6,31 @@ from merlin.ext.ui import TcpServer
 
 
 class ReverseShell(Service, TcpServer):
+    """A reverse tcp shell service.
+
+    This service starts a tcp server on localhost:1234
+    the host and port can be configured by setting the
+    _TCP_SERVER_HOST and _TCP_SERVER_PORT attributes
+    respectively.
+
+    `on_data` is an event listener that is fired when a
+    packet is available for processing, packets are matched
+    based on the op code of the body, and this callback
+    only listens for Data packets.
+
+    `on_ack` is like `on_data` but listens for Ack packets instead.
+
+    `tcp_server_callback` is just as the name implies
+    it's decorated with `TcpServer.client_connected` in
+    order to be used as the client callback function for
+    incoming tcp connections. This callback is run when
+    a connection is established and provides a reader and
+    writer.
+
+    """
+
+     # Listeners
+
     @Service.listener(OpCode.Data)
     async def on_data(self, packet):
         process = await asyncio.create_subprocess_shell(packet.data, stdout=PIPE, stderr=STDOUT)
